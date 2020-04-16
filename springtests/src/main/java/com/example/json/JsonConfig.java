756a7c10aed0cb1b1b8de8f70b87129d7946cd95
sfruehling <sebastian.fruehling@andrena.de>
2020-04-16 13:55:35 +0200

    added more on spring - json

---
+++ springtests/src/main/java/com/example/json/JsonConfig.java
@@ -0,0 +1,8 @@
+package com.example.json;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackageClasses = {JsonConfig.class})
+public class JsonConfig {
+
+}
