3be4f23e6ed45fee4d8be4403bdf57a7af3d4b77
sfruehling <sebastian.fruehling@andrena.de>
2020-07-28 15:46:04 +0200

    added Caching

---
+++ springtests2/src/test/java/sf/example/mockmvc/Starship.java
@@ -0,0 +1,17 @@
+package sf.example.mockmvc;
+
+public class Starship {
+
+	private String name;
+
+	public Starship() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ springtests2/src/test/java/sf/example/mockmvc/StarshipBookingControllerTest.java
@@ -0,0 +1,57 @@
+package sf.example.mockmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = StarshipBookingController.class)
+class StarshipBookingControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StarshipBookingService bookingService;
+
+    @Test
+    void callsServiceWithParameter_noResults() throws Exception {
+        when(bookingService.findBookableStarships(200)).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(
+                get("/bookable-starships")
+                        .header("capacity", "200")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isEmpty());
+    }
+
+    @Test
+    void callsServiceWithParameter_twoResults() throws Exception {
+        List<Starship> twoStarships = Arrays.asList(new Starship(), new Starship());
+
+        when(bookingService.findBookableStarships(42)).thenReturn(twoStarships);
+
+        mockMvc.perform(
+                get("/bookable-starships")
+                        .header("capacity", "42")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").value(hasSize(2)));
+    }
+
+}
